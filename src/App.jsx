69fa477eea057c77fc1793ff989ea51b87c29fe8
Naveen Vijayakumar <naveen26898@outlook.com>
2023-08-09 00:22:39 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, NAvbar, Tech, Works,StarsCanvas} from './components'; 
+const App= () => {
+ 
+  return (
+   <div>
+    
+   </div>
+  )
+}
+
+export default App
